598682f88580376c6a4464d16bbfb30f7c7b0a73
Sandro Neumann <sandro.neumann@idgis.nl>
2016-08-10 10:58:07 +0200

    roughly replaced templates in new tabs setup

--- imports/ui/step_2/step_2.js
+++ imports/ui/step_2/step_2.js
@@ -4,22 +4,14 @@ import './step_2.css';
 Template.step_2.onRendered(function() {
 	Session.set('stepNumber', '2');
 	
-	if(typeof Session.get('mapExtent') === 'undefined' || typeof Session.get('mapCenter') === 'undefined') {
-		var extent = [167658.241026781, 307862.821900462, 208090.624144334, 339455.907872023];
-		var center = [187000, 323000];
-	} else {
-		var extent = Session.get('mapExtent');
-		var center = Session.get('mapCenter');
-	}
-	
 	var projection = new ol.proj.Projection({
 		code: 'EPSG:28992',
-		extent: extent
+		extent: [167658.241026781, 307862.821900462, 208090.624144334, 339455.907872023]
 	});
 	
 	var view = new ol.View({
 		projection: projection,
-		center: center,
+		center: [187000, 323000],
 		zoom: 2
 	});
 	
@@ -31,9 +23,9 @@ Template.step_2.onRendered(function() {
 		view: view
 	});
 	
-	var url = Meteor.settings.public.landschapstypenService.url;
-	var layers = Meteor.settings.public.landschapstypenService.layers;
-	var version = Meteor.settings.public.landschapstypenService.version;
+	var url = Meteor.settings.public.deelgebiedenService.url;
+	var layers = Meteor.settings.public.deelgebiedenService.layers;
+	var version = Meteor.settings.public.deelgebiedenService.version;
 	
 	layers.forEach(function(item) {
 		var layer = new ol.layer.Image({
@@ -47,27 +39,53 @@ Template.step_2.onRendered(function() {
 	});
 	
 	map.on('singleclick', function(evt) {
-		var url = map.getLayers().item(Meteor.settings.public.landschapstypenService.indexLT)
+		var url = map.getLayers().item(Meteor.settings.public.deelgebiedenService.indexDG)
 				.getSource().getGetFeatureInfoUrl(evt.coordinate, map.getView().getResolution(), 
 				map.getView().getProjection(), {'INFO_FORMAT': 'application/vnd.ogc.gml'});
 		
-		Meteor.call('getLandschapsType', url, function(err, result) {
-			if(result === 'Dalbodem') {
-				Session.set('landschapstypeId', Meteor.settings.public.dalId);
-				Session.set('mapCoordinates', evt.coordinate);
-				Router.go('step_3');
-			} else if(result === 'Helling > 4 graden' || result === 'Helling < 4 graden') {
-				Session.set('landschapstypeId', Meteor.settings.public.hellingId);
-				Session.set('mapCoordinates', evt.coordinate);
-				Router.go('step_3');
-			} else if(result === 'Tussenterras' || result === 'Plateau' || result === 'Groeve' || 
-					result === 'Geisoleerde heuvel') {
-				Session.set('landschapstypeId', Meteor.settings.public.plateauId);
-				Session.set('mapCoordinates', evt.coordinate);
-				Router.go('step_3');
-			} else {
-				Session.set('landschapstypeId', null);
-				Session.set('mapCoordinates', null);
+		Meteor.call('getDeelgebied', url, function(err, result) {
+			if(result === 'Doenrade') {
+				var extent = [186871, 325595, 195806, 334149];
+				var center = [191338, 329872];
+			} else if(result === 'Schimmert') {
+				var extent = [179746, 319052, 190223,328561];
+				var center = [184984, 323806];
+			} else if(result === 'Margraten') {
+				var extent = [179143, 307181, 190422, 321341];
+				var center = [184782, 314261];
+			} else if(result === 'Maasterras Gronsveld') {
+				var extent = [178000, 312344, 182095, 321937];
+				var center = [180227, 317040];
+			} else if(result === 'Ubachsberg') {
+				var extent = [189833, 314940, 198236, 321304];
+				var center = [194034, 318122];
+			} else if(result === 'Eperheide') {
+				var extent = [188254, 306598, 193192, 316372];
+				var center = [191119, 310435];
+			} else if(result === 'Vijlenerbos') {
+				var extent = [192945, 306833, 200189, 313472];
+				var center = [196567, 310152];
+			} else if(result === 'Baneheide') {
+				var extent = [192636, 312343, 198545, 316508];
+				var center = [195590, 314425];
+			}
+			
+			if(result === 'Doenrade' || result === 'Schimmert' || result === 'Margraten' || 
+					result === 'Maasterras Gronsveld' || result === 'Ubachsberg' || result === 'Eperheide' ||
+					result === 'Vijlenerbos' || result === 'Baneheide') {
+				Session.set('mapExtent', extent);
+				Session.set('mapCenter', center);
+				
+				if(typeof Session.get('sectorId') === 'undefined') {
+					var sectorElement = $('#sector-dropdown-label');
+					$.each(sectorElement, function(index, item) {
+						$(item).css({'color':'#BF3F3F'});
+						$(item).css({'border':'1px solid black'});
+						$(item).css({'font-weight':'bold'});
+					});
+				} else {
+					Router.go('step_2');
+				}
 			}
 		});
 	});

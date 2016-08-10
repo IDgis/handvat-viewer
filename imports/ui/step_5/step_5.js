de379ec58e1501353fb5129fe2574fef8e8a3b7a
Sandro Neumann <sandro.neumann@idgis.nl>
2016-08-10 10:24:40 +0200

    added two extra steps/tabs

--- imports/ui/step_5/step_5.js
+++ imports/ui/step_5/step_5.js
@@ -1,2 +1,6 @@
 import './step_5.html';
 import './step_5.css';
+
+Template.step_5.onRendered(function() {
+	Session.set('stepNumber', '5');
+});
--- imports/ui/step_6/step_6.js
+++ imports/ui/step_6/step_6.js
@@ -1,2 +1,6 @@
 import './step_6.html';
 import './step_6.css';
+
+Template.step_6.onRendered(function() {
+	Session.set('stepNumber', '6');
+});

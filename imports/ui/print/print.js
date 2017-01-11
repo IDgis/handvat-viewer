8b2d457323ffbf09a9fdb1cfff7765c6683ce6eb
Sandro Neumann <sandro.neumann@idgis.nl>
2017-01-11 16:06:06 +0100

    created print page beginselen

--- imports/ui/print/print.js
+++ imports/ui/print/print.js
@@ -42,12 +42,15 @@ Template.print.onRendered(function() {
 	
 	var extent;
 	var center;
-	if(typeof Session.get('mapExtent') === 'undefined' || typeof Session.get('mapCoordinates') === 'undefined') {
+	var coordinates;
+	if(typeof Session.get('mapCoordinates') === 'undefined') {
 		extent = [165027, 306558, 212686, 338329];
 		center = [188856, 322443];
+		coordinates = [188856, 322443];
 	} else {
 		extent = Session.get('mapExtent');
-		center = Session.get('mapCoordinates');
+		center = Session.get('mapCenter');
+		coordinates = Session.get('mapCoordinates');
 	}
 	
 	var projection = new ol.proj.Projection({
@@ -55,13 +58,13 @@ Template.print.onRendered(function() {
 		extent: extent
 	});
 	
-	var view = new ol.View({
+	var viewLocationCentered = new ol.View({
 		projection: projection,
-		center: center,
+		center: coordinates,
 		zoom: 2
 	});
 	
-	var viewDeelgebied = new ol.View({
+	var viewCoordinateCentered = new ol.View({
 		projection: projection,
 		center: center,
 		zoom: 1
@@ -69,8 +72,20 @@ Template.print.onRendered(function() {
 	
 	var zoomControl = new ol.control.Zoom();
 	
-	addLocationMap(zoomControl, view);
-	addDeelgebiedMap(zoomControl, viewDeelgebied);
+	var top10Url = Meteor.settings.public.top10Service.url;
+	var top10Layers = Meteor.settings.public.top10Service.layers;
+	var top10Version = Meteor.settings.public.top10Service.version;
+	
+	var skUrl = Meteor.settings.public.landschapStructuurService.url;
+	var skLayerId = Meteor.settings.public.landschapStructuurService[Session.get('area')];
+	var skVersion = Meteor.settings.public.landschapStructuurService.version;
+	
+	addMapGroup(zoomControl, viewLocationCentered, 'map-print-location', top10Url, top10Layers, 
+			top10Version, true);
+	addMapLayer(zoomControl, viewCoordinateCentered, 'map-print-deelgebied', skUrl, skLayerId, 
+			skVersion, false);
+	addMapLayer(zoomControl, viewLocationCentered, 'map-print-beginselen', skUrl, skLayerId, 
+			skVersion, true);
 });
 
 Template.print.helpers({
@@ -123,83 +138,121 @@ Template.print.helpers({
 				});
 			}
 		});
+	},
+	getBeginselenIntro: function() {
+		HTTP.get(Meteor.settings.public.hostname + "/handvat-admin/text/json", {
+			headers: {
+				'Content-Type' : 'application/json; charset=UTF-8'
+			}
+		}, function(err, result) {
+			Meteor.call('getTextFromCoupling', result.content, Meteor.settings.public.stap3Beginselen, function(err, result) {
+				if(typeof result !== 'undefined') {
+					$('#print-beginselen-general').append(result.contentPrint);
+				}
+			});
+		});
+	},
+	getBeginselen: function() {
+		if(typeof Session.get('landschapstypeId') !== 'undefined' && Session.get('landschapstypeId') !== null) {
+			var count = 1;
+			
+			HTTP.get(Meteor.settings.public.hostname + "/handvat-admin/coupling/leidend/json", {
+				headers: {
+					'Content-Type' : 'application/json; charset=UTF-8'
+				}
+			}, function(err, result) {
+				Meteor.call('getBeginselen', result.content, Session.get('landschapstypeId'), function(err, result) {
+					$.each(result, function(index, item) {
+						HTTP.get(Meteor.settings.public.hostname + "/handvat-admin/text/json", {
+							headers: {
+								'Content-Type' : 'application/json; charset=UTF-8'
+							}
+						}, function(err, result) {
+							Meteor.call('getTextFromId', result.content, item, function(err, result) {
+								if(typeof result !== 'undefined') {
+									$('#print-lb-' + count).append(result.contentPrint);
+									count++;
+								}
+							});
+						});
+					});
+				});
+			});
+		}
 	}
 });
 
-function addLocationMap(zoomControl, view) {
-	var mapLocation = new ol.Map({
+function addMapGroup(zoomControl, view, target, url, layers, version, setMarker) {
+	var map = new ol.Map({
 		control: zoomControl,
-		target: 'map-print-location',
+		target: target,
 		view: view
 	});
 	
-	var urlTop10 = Meteor.settings.public.top10Service.url;
-	var layersTop10 = Meteor.settings.public.top10Service.layers;
-	var versionTop10 = Meteor.settings.public.top10Service.version;
-	
-	layersTop10.forEach(function(item) {
+	layers.forEach(function(item) {
 		var layer = new ol.layer.Image({
 			source: new ol.source.ImageWMS({
-				url: urlTop10, 
-				params: {'LAYERS': item, 'VERSION': versionTop10} 
+				url: url, 
+				params: {'LAYERS': item, 'VERSION': version} 
 			})
 		});
 		
-		mapLocation.addLayer(layer);
+		map.addLayer(layer);
 	});
 	
-	if(Session.get('mapCoordinates') !== null && typeof Session.get('mapCoordinates') !== 'undefined') {
-		var iconLayer = getIcon(Session.get('mapCoordinates'));
-		mapLocation.addLayer(iconLayer);
+	if(setMarker) {
+		setIcon(map, Session.get('mapCoordinates'));
 	}
 }
 
-function addDeelgebiedMap(zoomControl, view) {
-	var mapDeelgebied = new ol.Map({
+function addMapLayer(zoomControl, view, target, url, id, version, setMarker) {
+	var map = new ol.Map({
 		control: zoomControl,
-		target: 'map-print-deelgebied',
+		target: target,
 		view: view
 	});
 	
-	var url = Meteor.settings.public.landschapStructuurService.url;
-	var version = Meteor.settings.public.landschapStructuurService.version;
-	var layerId = Meteor.settings.public.landschapStructuurService[Session.get('area')];
-	
-	var areaLayer = new ol.layer.Image({
+	var layer = new ol.layer.Image({
 		source: new ol.source.ImageWMS({
 			url: url, 
-			params: {'LAYERS': Meteor.settings.public.landschapStructuurService.layers[layerId], 
+			params: {'LAYERS': Meteor.settings.public.landschapStructuurService.layers[id], 
 				'VERSION': version} 
 		})
 	});
+
+	map.addLayer(layer);
 	
-	mapDeelgebied.addLayer(areaLayer);
+	if(setMarker) {
+		setIcon(map, Session.get('mapCoordinates'));
+	}
 }
 
-function getIcon(coordinates) {
-	var iconStyle = new ol.style.Style({
-		image: new ol.style.Icon(({
-			anchor: [0.5, 32],
-			anchorXUnits: 'fraction',
-			anchorYUnits: 'pixels',
-			opacity: 0.75,
-			src: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port +
-				'/' +  Meteor.settings.public.domainSuffix + '/images/location.svg',
-			size: [32, 32]
-		}))
-	});
-	
-	var iconFeature = new ol.Feature({
-		geometry: new ol.geom.Point(coordinates)
-	});
-	
-	var vectorLayer = new ol.layer.Vector({
-		source: new ol.source.Vector({
-			features: [iconFeature]
-		})
-	});
-	
-	iconFeature.setStyle(iconStyle);
-	
-	return vectorLayer;
+function setIcon(map, coordinates) {
+	if(Session.get('mapCoordinates') !== null && typeof Session.get('mapCoordinates') !== 'undefined') {
+		var iconStyle = new ol.style.Style({
+			image: new ol.style.Icon(({
+				anchor: [0.5, 32],
+				anchorXUnits: 'fraction',
+				anchorYUnits: 'pixels',
+				opacity: 0.75,
+				src: window.location.protocol + '//' + window.location.hostname + ':' + window.location.port +
+					'/' +  Meteor.settings.public.domainSuffix + '/images/location.svg',
+				size: [32, 32]
+			}))
+		});
+		
+		var iconFeature = new ol.Feature({
+			geometry: new ol.geom.Point(coordinates)
+		});
+		
+		iconFeature.setStyle(iconStyle);
+		
+		var vectorLayer = new ol.layer.Vector({
+			source: new ol.source.Vector({
+				features: [iconFeature]
+			})
+		});
+		
+		map.addLayer(vectorLayer);
+	}
 }
